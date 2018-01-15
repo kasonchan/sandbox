d8fc1f508a1fbc3c9a74a289f1caf1dd16b6f807
kasonchan <kasonl.chan@gmail.com>
2018-01-15 21:54:26 +0000

    Initial commit akka-typed

---
+++ akka-typed/README.md
@@ -0,0 +1,5 @@
+# akka-typed
+
+This repository is for playing with Akka Typed.
+
+
---
+++ akka-typed/src/main/scala/Main.scala
@@ -0,0 +1,11 @@
+/**
+  * @author kasonchan
+  * @since Dec-2017
+  */
+object Main {
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+}
