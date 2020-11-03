b120eff95df05b7075ed527d2551f90193036a7f
kasonchan <kasonl.chan@gmail.com>
2020-11-03 21:29:11 +0000

    Add logrus and uuidgen traceId #35

--- go-server/go-server.go
+++ go-server/go-server.go
@@ -3,13 +3,34 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os/exec"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	http.HandleFunc("/", HelloServer)
+	log.SetFormatter(&log.JSONFormatter{})
+	log.SetLevel(log.InfoLevel)
+
+	http.HandleFunc("/", Server)
 	http.ListenAndServe(":8080", nil)
 }
 
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+func Server(w http.ResponseWriter, r *http.Request) {
+	out, err := exec.Command("uuidgen").Output()
+	if err != nil {
+		log.WithFields(log.Fields{
+			"path":    r.URL.Path[1:],
+			"traceId": err,
+		}).Error(err)
+		// TODO: Status code 500 due to not able to generate uuid
+	}
+
+	if out != nil {
+		log.WithFields(log.Fields{
+			"path":    r.URL.Path[1:],
+			"traceId": out,
+		}).Info("Successful")
+		fmt.Fprintf(w, "%s", r.URL.Path[1:])
+	}
 }
